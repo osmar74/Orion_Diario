17db4204cf218aae0f0b0ec152a897adf1153f72
osmar74 <orat74@gmail.com>
2026-05-17 11:19:24 -0400

    Format Flask app entrypoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,20 @@
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'orion_secret_key_2026'
+    app.secret_key = "orion_secret_key_2026"
 
     from app.controllers.main_blueprint import main_bp
-    app.register_blueprint(main_bp)
-
     from app.controllers.fases_blueprint import fases_ab_bp
-    app.register_blueprint(fases_ab_bp)
-
     from app.controllers.ocr_blueprint import ocr_bp
-    app.register_blueprint(ocr_bp)
-
     from app.controllers.procesamiento_blueprint import proc_bp
-    app.register_blueprint(proc_bp)
-
     from app.controllers.carga_blueprint import carga_bp
+
+    app.register_blueprint(main_bp)
+    app.register_blueprint(fases_ab_bp)
+    app.register_blueprint(ocr_bp)
+    app.register_blueprint(proc_bp)
     app.register_blueprint(carga_bp)
 
     return app
