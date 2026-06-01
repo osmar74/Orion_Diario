de0de3d3a1cc8dad525b8ebb5fa085b3fb3190a7
osmar74 <orat74@gmail.com>
2026-06-01 11:15:06 -0400

    Limpiar tools temporales y ajustar SQL gestion ASTER

--- app/sql/aster/gestion/export_gestion_aster.sql
+++ app/sql/aster/gestion/export_gestion_aster.sql
@@ -1,197 +1,322 @@
-WITH
-CTE_Aster_Base AS (
-    SELECT
-        Codigo,
-        Fecha_Hora,
-        Duracion,
-        Estado,
-        Atendio,
-        Numero,
-        Cartera,
-        CASE
-            WHEN Estado = 'ATENDIDO'
-             AND Atendio IN ('HUMANO', 'DESCONOCIDO')
-            THEN 1 ELSE 0
-        END AS EsHumano
-    FROM [dbo].[aster_dia_nc]
-    WHERE Fecha_Hora >= ? AND Fecha_Hora < ?
-),
+USE [Aster_Api]
 
-CTE_Comentarios_Base AS (
-    SELECT
-        [data],
-        resultado1,
-        resultado2,
-        datapers,
-        usuario,
-        comentario
-    FROM [dbo].[comentarios]
-    WHERE fecha >= ? AND fecha < ?
-),
+GO
 
-CTE_Maquinas AS (
-    SELECT DISTINCT
-        Codigo AS Cliente_Nro,
-        Fecha_Hora,
-        Duracion,
-        CASE
-            WHEN Estado = 'OCUPADO' AND Atendio = 'NULL' THEN 'Telefono Ocupado'
-            WHEN Estado = 'ATENDIDO' AND Atendio = 'CONTESTADOR' THEN 'Buzon de voz'
-            WHEN Estado = 'NO ATENDIDO' AND Atendio = 'NULL' THEN 'No contestan'
-            WHEN Estado = 'ATENDIDO' AND Atendio = 'CORTO' THEN 'No contestan'
-            WHEN Estado = 'CONGESTION' AND Atendio = 'NULL' THEN 'Telefono Fuera de Servicio'
-            WHEN Estado = 'SIN CANALES' AND Atendio = 'NULL' THEN 'Telefono Fuera de Servicio'
-            ELSE 'Usuario pide volver a llamar'
-        END AS [Descripcion Codigo De Gestion],
-        '' AS Fecha_Compromiso,
-        '' AS Grabador,
-        Numero AS Telefonos,
-        CASE
-            WHEN Codigo IS NULL OR LTRIM(RTRIM(Codigo)) = '' THEN NULL
-            WHEN Codigo LIKE 'M%' THEN 'Mobile'
-            ELSE 'Home'
-        END AS [Tipo Cartera],
-        '' AS Asesor,
-        Cartera AS [Antiguedad De La Cartera],
-        '' AS [Nota de la Gestion],
-        'codmaquina' AS [Clase de Gestion],
-        '' AS [Causal de Mora/Respuesta]
-    FROM CTE_Aster_Base
-    WHERE EsHumano = 0
-),
 
-CTE_Humanos_Processed AS (
-    SELECT
-        _dia.Codigo AS Cliente_Nro,
-        _dia.Fecha_Hora,
-        _dia.Duracion,
-        _dia.Numero AS Telefonos,
-        _dia.Cartera AS [Antiguedad De La Cartera],
-        CASE
-            WHEN _contactada.resultado1 IS NULL
-              OR LTRIM(RTRIM(_contactada.resultado1)) = ''
-            THEN 'codmaquina'
-            ELSE 'TEL'
-        END AS [Clase de Gestion],
 
-        LTRIM(RTRIM(CASE
-            WHEN _contactada.resultado1 IS NULL
-              OR LTRIM(RTRIM(_contactada.resultado1)) = ''
-            THEN
-                CASE
-                    WHEN _dia.Estado = 'OCUPADO' AND _dia.Atendio = 'NULL' THEN 'Telefono Ocupado'
-                    WHEN _dia.Estado = 'ATENDIDO' AND _dia.Atendio = 'CONTESTADOR' THEN 'Buzon de voz'
-                    WHEN _dia.Estado = 'NO ATENDIDO' AND _dia.Atendio = 'NULL' THEN 'No contestan'
-                    WHEN _dia.Estado = 'ATENDIDO' AND _dia.Atendio = 'CORTO' THEN 'No contestan'
-                    WHEN _dia.Estado = 'CONGESTION' AND _dia.Atendio = 'NULL' THEN 'Telefono Fuera de Servicio'
-                    WHEN _dia.Estado = 'SIN CANALES' AND _dia.Atendio = 'NULL' THEN 'Telefono Fuera de Servicio'
-                    ELSE 'Usuario pide volver a llamar'
-                END
-            ELSE [dbo].[Obtener_Estado](_contactada.resultado1, _contactada.resultado2)
-        END)) AS [Desc_Raw],
+BEGIN
 
-        CASE
-            WHEN CHARINDEX('compromisos<=>', _contactada.datapers) > 0 THEN
-                REPLACE(
-                    SUBSTRING(
-                        _contactada.datapers,
-                        CHARINDEX('compromisos<=>', _contactada.datapers) + 14,
-                        CASE
-                            WHEN CHARINDEX('###', _contactada.datapers, CHARINDEX('compromisos<=>', _contactada.datapers)) > 0
-                            THEN CHARINDEX('###', _contactada.datapers, CHARINDEX('compromisos<=>', _contactada.datapers))
-                               - (CHARINDEX('compromisos<=>', _contactada.datapers) + 14)
-                            ELSE 10
-                        END
-                    ),
-                    '-',
-                    '/'
-                )
-            ELSE ''
-        END AS Fecha_Compromiso,
+    SET NOCOUNT ON;
 
-        [dbo].[Obtener_Usuario](_contactada.usuario) AS Grabador,
 
-        CASE
-            WHEN _dia.Codigo IS NULL OR LTRIM(RTRIM(_dia.Codigo)) = '' THEN NULL
-            WHEN _dia.Codigo LIKE 'M%' THEN 'Mobile'
-            ELSE 'Home'
-        END AS [Tipo Cartera],
 
-        _contactada.usuario AS Asesor,
+    DECLARE @Fecha DATE = '2026-04-29';
 
-        LTRIM(RTRIM(
-            REPLACE(
-                REPLACE(
-                    REPLACE(
-                        REPLACE(_contactada.comentario, CHAR(9), ''),
-                        CHAR(10),
-                        ''
-                    ),
-                    CHAR(13),
-                    ''
-                ),
-                CHAR(160),
-                ' '
-            )
-        )) AS [Nota_Clean],
+    
 
-        LTRIM(RTRIM([dbo].[Obtener_Causal_Mora](_contactada.resultado2)))
-            AS [Causal de Mora/Respuesta]
-    FROM CTE_Aster_Base _dia
-    LEFT JOIN CTE_Comentarios_Base _contactada
-        ON _dia.Codigo = _contactada.[data]
-    WHERE _dia.EsHumano = 1
-),
+    -- Optimizamos el filtro de fecha para que sea SARGABLE (usa índices de forma nativa)
 
-CTE_Humanos_Final AS (
-    SELECT DISTINCT
-        Cliente_Nro,
-        Fecha_Hora,
-        Duracion,
-        CASE
-            WHEN [Desc_Raw] = 'Usuario pide volver a llamar'
-             AND [Clase de Gestion] = 'codmaquina'
-            THEN 'No contestan'
-            ELSE [Desc_Raw]
-        END AS [Descripcion Codigo De Gestion],
+    DECLARE @FechaInicio DATETIME = CAST(@Fecha AS DATETIME);
+
+    DECLARE @FechaFin DATETIME = DATEADD(DAY, 1, @FechaInicio);
+
+
+
+    WITH 
+
+    -- 1. Extraemos y clasificamos las llamadas en una sola lectura de disco
+
+    CTE_Aster_Base AS (
+
+        SELECT 
+
+            Codigo,
+
+            Fecha_Hora,
+
+            Duracion,
+
+            Estado,
+
+            Atendio,
+
+            Numero,
+
+            Cartera,
+
+            CASE WHEN Estado = 'ATENDIDO' AND Atendio IN ('HUMANO', 'DESCONOCIDO') THEN 1 ELSE 0 END AS EsHumano
+
+        FROM [dbo].[aster_dia_nc]
+
+        WHERE Fecha_Hora >= @FechaInicio AND Fecha_Hora < @FechaFin
+
+    ),
+
+
+
+    -- 2. Filtramos los comentarios del día de forma eficiente
+
+    CTE_Comentarios_Base AS (
+
+        SELECT 
+
+            [data],
+
+            resultado1,
+
+            resultado2,
+
+            datapers,
+
+            usuario,
+
+            comentario
+
+        FROM [dbo].[comentarios]
+
+        WHERE fecha >= @FechaInicio AND fecha < @FechaFin
+          AND ISNULL(LTRIM(RTRIM(usuario)), '') <> 'SystemUser'
+
+    ),
+
+
+
+    -- 3. PROCESAMIENTO: Llamadas No Atendidas por Humanos (Máquinas)
+
+    CTE_Maquinas AS (
+
+        SELECT DISTINCT
+
+            Codigo AS Cliente_Nro,
+
+            Fecha_Hora,
+
+            Duracion,
+
+            CASE
+
+                WHEN Estado = 'OCUPADO' AND Atendio = 'NULL' THEN 'Telefono Ocupado'
+
+                WHEN Estado = 'ATENDIDO' AND Atendio = 'CONTESTADOR' THEN 'Buzon de voz'
+
+                WHEN Estado = 'NO ATENDIDO' AND Atendio = 'NULL' THEN 'No contestan'
+
+                WHEN Estado = 'ATENDIDO' AND Atendio = 'CORTO' THEN 'No contestan'
+
+                WHEN Estado = 'CONGESTION' AND Atendio = 'NULL' THEN 'Telefono Fuera de Servicio'
+
+                WHEN Estado = 'SIN CANALES' AND Atendio = 'NULL' THEN 'Telefono Fuera de Servicio'
+
+                ELSE 'Usuario pide volver a llamar'
+
+            END AS [Descripcion Codigo De Gestion],
+
+            '' AS Fecha_Compromiso,
+
+            '' AS Grabador,
+
+            Numero AS Telefonos,
+
+            CASE WHEN Codigo IS NULL OR LTRIM(RTRIM(Codigo)) = '' THEN NULL WHEN Codigo LIKE 'M%' THEN 'Mobile' ELSE 'Home' END AS [Tipo Cartera],
+
+            '' AS Asesor,
+
+            Cartera AS [Antiguedad De La Cartera],
+
+            '' AS [Nota de la Gestion],
+
+            'codmaquina' AS [Clase de Gestion],
+
+            '' AS [Causal de Mora/Respuesta]
+
+        FROM CTE_Aster_Base
+
+        WHERE EsHumano = 0
+
+    ),
+
+
+
+    -- 4. PROCESAMIENTO: Llamadas Atendidas por Humanos (Con Cruce de Comentarios)
+
+    CTE_Humanos_Processed AS (
+
+        SELECT 
+
+            _dia.Codigo AS Cliente_Nro,
+
+            _dia.Fecha_Hora,
+
+            _dia.Duracion,
+
+            _dia.Numero AS Telefonos,
+
+            _dia.Cartera AS [Antiguedad De La Cartera],
+
+            CASE WHEN _contactada.resultado1 IS NULL OR LTRIM(RTRIM(_contactada.resultado1)) = '' THEN 'codmaquina' ELSE 'TEL' END AS [Clase de Gestion],
+
+            
+
+            -- Evaluación base de la descripción
+
+            LTRIM(RTRIM(CASE
+
+                WHEN _contactada.resultado1 IS NULL OR LTRIM(RTRIM(_contactada.resultado1)) = '' THEN
+
+                    CASE
+
+                        WHEN _dia.Estado = 'OCUPADO' AND _dia.Atendio = 'NULL' THEN 'Telefono Ocupado'
+
+                        WHEN _dia.Estado = 'ATENDIDO' AND _dia.Atendio = 'CONTESTADOR' THEN 'Buzon de voz'
+
+                        WHEN _dia.Estado = 'NO ATENDIDO' AND _dia.Atendio = 'NULL' THEN 'No contestan'
+
+                        WHEN _dia.Estado = 'ATENDIDO' AND _dia.Atendio = 'CORTO' THEN 'No contestan'
+
+                        WHEN _dia.Estado = 'CONGESTION' AND _dia.Atendio = 'NULL' THEN 'Telefono Fuera de Servicio'
+
+                        WHEN _dia.Estado = 'SIN CANALES' AND _dia.Atendio = 'NULL' THEN 'Telefono Fuera de Servicio'
+
+                        ELSE 'Usuario pide volver a llamar'
+
+                    END
+
+                ELSE [dbo].[Obtener_Estado](_contactada.resultado1, _contactada.resultado2)
+
+            END)) AS [Desc_Raw],
+
+            
+
+            CASE 
+
+                WHEN CHARINDEX('compromisos<=>', _contactada.datapers) > 0 THEN
+
+                    REPLACE(SUBSTRING(_contactada.datapers, CHARINDEX('compromisos<=>', _contactada.datapers) + 14,
+
+                            CASE WHEN CHARINDEX('###', _contactada.datapers, CHARINDEX('compromisos<=>', _contactada.datapers)) > 0 
+
+                                 THEN CHARINDEX('###', _contactada.datapers, CHARINDEX('compromisos<=>', _contactada.datapers)) - (CHARINDEX('compromisos<=>', _contactada.datapers) + 14)
+
+                                 ELSE 10 END), '-', '/')
+
+                ELSE ''
+
+            END AS Fecha_Compromiso,
+
+            [dbo].[Obtener_Usuario](_contactada.usuario) AS Grabador,
+
+            CASE WHEN _dia.Codigo IS NULL OR LTRIM(RTRIM(_dia.Codigo)) = '' THEN NULL WHEN _dia.Codigo LIKE 'M%' THEN 'Mobile' ELSE 'Home' END AS [Tipo Cartera],
+
+            _contactada.usuario AS Asesor,
+
+            
+
+            -- Limpieza de caracteres especiales centralizada
+
+            LTRIM(RTRIM(REPLACE(REPLACE(REPLACE(REPLACE(_contactada.comentario, CHAR(9), ''), CHAR(10), ''), CHAR(13), ''), CHAR(160), ' '))) AS [Nota_Clean],
+
+            LTRIM(RTRIM([dbo].[Obtener_Causal_Mora](_contactada.resultado2))) AS [Causal de Mora/Respuesta]
+
+        FROM CTE_Aster_Base _dia
+
+        LEFT JOIN CTE_Comentarios_Base _contactada ON _dia.Codigo = _contactada.[data] 
+
+        WHERE _dia.EsHumano = 1
+
+    ),
+
+
+
+    CTE_Humanos_Final AS (
+
+        SELECT DISTINCT
+
+            Cliente_Nro,
+
+            Fecha_Hora,
+
+            Duracion,
+
+            CASE WHEN [Desc_Raw] = 'Usuario pide volver a llamar' AND [Clase de Gestion] = 'codmaquina' THEN 'No contestan' ELSE [Desc_Raw] END AS [Descripcion Codigo De Gestion],
+
+            Fecha_Compromiso,
+
+            Grabador,
+
+            Telefonos,
+
+            [Tipo Cartera],
+
+            Asesor,
+
+            [Antiguedad De La Cartera],
+
+            CASE WHEN [Desc_Raw] = 'Usuario pide volver a llamar' AND [Clase de Gestion] = 'codmaquina' THEN '' ELSE [Nota_Clean] END AS [Nota de la Gestion],
+
+            [Clase de Gestion],
+
+            [Causal de Mora/Respuesta]
+
+        FROM CTE_Humanos_Processed
+
+    ),
+
+
+
+    -- 5. UNIÓN DE UNIVERSOS
+
+    CTE_Universo AS (
+
+        SELECT * FROM CTE_Maquinas
+
+        UNION ALL
+
+        SELECT * FROM CTE_Humanos_Final
+
+    )
+
+
+
+    -- 6. SELECT FINAL CON FORMATEOS DE INTERFAZ (Se ejecutan una sola vez al final)
+
+    SELECT 
+
+        Cliente_Nro AS [Cliente Nro.],
+
+        FORMAT(Fecha_Hora, 'dd/MM/yyyy') AS [Fecha De Gestion],
+
+        ISNULL(CONVERT(VARCHAR(8), Fecha_Hora, 108), '00:00:00') AS [Hora De Gestion],
+
+        FORMAT(DATEADD(SECOND, COALESCE(TRY_CAST(Duracion AS INT), 0), 0), 'HH:mm:ss') AS [Duracion llamada],
+
+        [Descripcion Codigo De Gestion],
+
         Fecha_Compromiso,
+
         Grabador,
+
+        'Vencorp' AS [Responsable De Cobro],
+
         Telefonos,
+
         [Tipo Cartera],
+
         Asesor,
+
         [Antiguedad De La Cartera],
-        CASE
-            WHEN [Desc_Raw] = 'Usuario pide volver a llamar'
-             AND [Clase de Gestion] = 'codmaquina'
-            THEN ''
-            ELSE [Nota_Clean]
-        END AS [Nota de la Gestion],
+
+        [Nota de la Gestion],
+
         [Clase de Gestion],
+
         [Causal de Mora/Respuesta]
-    FROM CTE_Humanos_Processed
-),
 
-CTE_Universo AS (
-    SELECT * FROM CTE_Maquinas
-    UNION ALL
-    SELECT * FROM CTE_Humanos_Final
-)
+    FROM CTE_Universo
 
-SELECT
-    Cliente_Nro AS [Cliente Nro.],
-    FORMAT(Fecha_Hora, 'dd/MM/yyyy') AS [Fecha De Gestion],
-    ISNULL(CONVERT(VARCHAR(8), Fecha_Hora, 108), '00:00:00') AS [Hora De Gestion],
-    FORMAT(DATEADD(SECOND, COALESCE(TRY_CAST(Duracion AS INT), 0), 0), 'HH:mm:ss') AS [Duracion llamada],
-    [Descripcion Codigo De Gestion],
-    Fecha_Compromiso,
-    Grabador,
-    'Vencorp' AS [Responsable De Cobro],
-    Telefonos,
-    [Tipo Cartera],
-    Asesor,
-    [Antiguedad De La Cartera],
-    [Nota de la Gestion],
-    [Clase de Gestion],
-    [Causal de Mora/Respuesta]
-FROM CTE_Universo
-ORDER BY [Clase de Gestion], [Hora De Gestion]
+    ORDER BY [Clase de Gestion], [Hora De Gestion];
+
+
+
+END
+
+GO
